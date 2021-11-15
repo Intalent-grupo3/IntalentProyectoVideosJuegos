e72a56de950e0e609593d4246fcbd092a6022fea
yelder <yelder302@gmail.com>
2021-11-15 18:38:39 +0100

    Se ha agregado nuevas funciones

--- src/js/recogida-input.js
+++ src/js/recogida-input.js
@@ -1,3 +1,10 @@
+
+/*Puntos para no olvidar mañana xD
+Se cargan bien las palabras del api y al jugar funciona bien el match y va aumentando la puntuacion
+pero cuando se termina el tiempo, se puede seguir jugando, también falta lo de seleccionar la dificultad
+ya que cuando se selecciona genera una palabra nueva pero no se cambia el tiempo y sigue corriendo el tiempo de la otra dificultad.*/
+
+
 let puntuacion = 0;
 let seguirJugando;
 let maximaPuntuacion;
@@ -9,7 +16,7 @@ const niveles = {
 let nivelActual = niveles.facil;
 
 const palabraInput = document.querySelector('#palabra-input');
-const palabraActual = document.querySelector('#palabra-actual');
+let palabraActual = document.querySelector('#palabra-actual');
 const mensaje = document.querySelector('#message');
 const segundos = document.querySelector('#segundos');
 const puntuacionDisplay = document.querySelector('#score');
@@ -45,10 +52,11 @@ function iniciarJuego() {
     //Se agregan los segundos en el display
     segundos.innerHTML = nivelActual;
     //Se cargan las palabras
-    getPalabra().palabraActual;
-    console.log(palabraActual);
-
+    getPalabra();
     palabraInput.addEventListener('input', empezarJuego);
+    setInterval(statusJugador, 50);
+    maximaPuntuacion = localStorage.getItem('maximapuntuacion');
+    puntuacionDisplay.innerHTML = maximaPuntuacion;
 
 }
 iniciarJuego();
@@ -57,13 +65,13 @@ function empezarJuego() {
     palabraInput.value = palabraInput.value.toLowerCase();
     if (palabrasCorrectas()) {
         seguirJugando = true;
-        getPalabra();
+        getPalabra(palabraActual);
         palabraInput.value = '';
         puntuacion++;
     }
 
     // Si no se logra escribir la palabra a tiempo
-    if (puntacion === -1) {
+    if (puntuacion === -1) {
         puntuacionDisplay.innerHTML = 0;
     } else {
         puntuacionDisplay.innerHTML = puntuacion;
@@ -73,22 +81,45 @@ function empezarJuego() {
             localStorage.setItem('maximaPuntuacion', puntuacion);
         }
     }
-    maximaPuntuacion = localStorage.getItem('');
+    maximaPuntuacion = localStorage.getItem('maximaPuntuacion');
     puntuacionDisplay.innerHTML = puntuacion;
     puntuacionMaxima.innerHTML = maximaPuntuacion;
 }
 
 
 function palabrasCorrectas() {
-    getPalabra()
-    if (palabraInput.value === palabraActual.innerHTML) {
+    //Funcion de comparar las palabras lanzadas por el api con las escritas por el cliente
+    if (palabraInput.value === palabraActual) {
         mensaje.innerHTML = 'Excelente, sigue con otra palabra 😀';
         return true;
     } else {
-        mensaje.innerHTML = 'Mal muy mal Lechón 🙄';
+        mensaje.innerHTML = '🙄';
         return false;
     }
 }
 
+function temporizador() {
+    var segundos = 30;
+    setInterval(function () {
 
+        document.getElementById("temporizador").innerHTML = segundos;
+        segundos--;
+        if (segundos <= 0) {
+            document.getElementById("temporizador").innerHTML = "TIEMPO COMPLETADO";
 
+        }
+
+    }, 1000);
+}
+temporizador()
+
+function statusJugador() {
+    if (!seguirJugando && segundos === 0) {
+        message.innerHTML = 'Haz perdido! 😞😞😞';
+        score = -1;
+    }
+}
+
+facilBtn.addEventListener('click', seleccionarNivel);
+medioBtn.addEventListener('click', seleccionarNivel);
+dificilBtn.addEventListener('click', seleccionarNivel);

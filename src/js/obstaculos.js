9fe9e7773111ce5aee1426623e8c4d999af2f112
callmeferre <callmeferre@gmail.com>
2021-11-15 17:51:44 +0100

    función collision

--- src/js/obstaculos.js
+++ src/js/obstaculos.js
@@ -18,6 +18,9 @@ let gameArea = {
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
+    end: function () {
+        clearInterval(this.interval);
+    },
 };
 
 function component(width, height, color, x, y) {
@@ -33,11 +36,37 @@ function component(width, height, color, x, y) {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     };
+    this.collision = function (obstacle) {
+        let left = this.x;
+        let right = this.x + this.width;
+        let top = this.y;
+        let bottom = this.y + this.height;
+        let obstacleleft = obstacle.x;
+        let obstacleright = obstacle.x + obstacle.width;
+        let obstacletop = obstacle.y;
+        let obstaclebottom = obstacle.y + obstacle.height;
+        let crash = true;
+        if (
+            bottom < obstacletop ||
+            top > obstaclebottom ||
+            right < obstacleleft ||
+            left > obstacleright
+        ) {
+            crash = false;
+        }
+        return crash;
+    };
 }
 
 function updateGameArea() {
     gameArea.clear();
     gameArea.frameNo += 1;
+    for (i = 0; i < obstacles.length; i += 1) {
+        if (personaje.collision(obstacles[i])) {
+            gameArea.end();
+            return;
+        }
+    }
     if (gameArea.frameNo == 1 || everyinterval(200)) {
         x = gameArea.canvas.width;
         y = Math.random() * (gameArea.canvas.height - 50);

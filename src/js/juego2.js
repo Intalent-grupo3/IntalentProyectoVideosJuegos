d18a9c6c673046a71c0d573768963997e355ba64
Apulsar <themrmike98@hotmail.es>
2021-11-17 13:20:54 +0100

    terminados buffs y debuffs a falta de imágenes

--- src/js/juego2.js
+++ src/js/juego2.js
@@ -6,60 +6,69 @@ let fondo;
 let puntos = 0;
 let hora;
 let puntuacion;
-let leaderscore = ['', '', '', '', ''];
-let leaderplayer = ['', '', '', '', ''];
+let leaderscore = ["", "", "", "", ""];
+let leaderplayer = ["", "", "", "", ""];
 let velocidad;
 let dist;
 let registrodist;
-let score = 1;
+let score;
+let sizeChange;
+let playerWidth;
+let playerHeigth;
+let dificultad;
 let bufferCond = 0;
 // -----------------------------------------------------------------------Inicio del juego
 function startGame() {
     velocidad = 10;
     dist = 1.5;
+    sizeChange = 1;
+    score = 1;
+    playerHeigth = 50;
+    playerWidth = 50;
+    dificultad = 0;
     gameArea.start();
     personaje = new component(
-        50,
-        50,
-        './images/juego2/rocket.svg',
+        playerWidth,
+        playerHeigth,
+        "./images/juego2/rocket.svg",
         20,
         220,
-        'player'
+        "player"
     );
     fondo = new component(
         1800,
         490,
-        './images/juego2/ba.webp',
+        "./images/juego2/ba.webp",
         1,
         1,
-        'background'
+        "background"
     );
     resetcronometro();
     iniciarcronometro();
-    document.querySelector('#menu').style.display = 'none';
-    document.querySelector('#imagenBox').style.display = 'none';
-    if (document.querySelector('#leadList')) {
-        leaderboard.removeChild(document.querySelector('#leadList'));
+    document.querySelector("#menu").style.display = "none";
+    document.querySelector("#imagenBox").style.display = "none";
+    if (document.querySelector("#leadList")) {
+        leaderboard.removeChild(document.querySelector("#leadList"));
     }
 }
 
 let gameArea = {
-    canvas: document.querySelector('#juego2'),
+    canvas: document.querySelector("#juego2"),
     // -------------------------------------------------------------------Estructura del lienzo
     start: function () {
         console.log(buffers);
         this.canvas.width = 640;
         this.canvas.height = 490;
-        this.context = this.canvas.getContext('2d');
+        this.context = this.canvas.getContext("2d");
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, velocidad);
         // ---------------------------------------------------------------Evento de teclado
-        window.addEventListener('keydown', function (e) {
+        window.addEventListener("keydown", function (e) {
             gameArea.keys = gameArea.keys || [];
-            gameArea.keys[e.key] = e.type == 'keydown';
+            gameArea.keys[e.key] = e.type == "keydown";
         });
-        window.addEventListener('keyup', function (e) {
-            gameArea.keys[e.key] = e.type == 'keydown';
+        window.addEventListener("keyup", function (e) {
+            gameArea.keys[e.key] = e.type == "keydown";
         });
     },
     // --------------------------------------------------------------------Clear (deja el canvas en blanco)
@@ -75,8 +84,8 @@ let gameArea = {
         buffers = [];
         bufferCond = 0;
         checkleaderboard();
-        document.querySelector('#botoninicio').value = 'reiniciar';
-        document.querySelector('#menu').style.display = 'block';
+        document.querySelector("#botoninicio").value = "reiniciar";
+        document.querySelector("#menu").style.display = "block";
     },
 };
 // --------------------------------------------------------------------------Declaración de componente
@@ -87,16 +96,16 @@ function component(width, height, color, x, y, type, bufferNum) {
     this.y = y;
     this.type = type;
     this.bufferNum = bufferNum;
-    if (type == 'player' || type == 'obstacle' || type == 'background') {
+    if (type == "player" || type == "obstacle" || type == "background") {
         this.image = new Image();
         this.image.src = color;
     }
     ctx = gameArea.context;
     // ------------------------------------------------------------------------Repintado de componente
     this.update = function () {
-        if (type == 'player' || type == 'obstacle' || type == 'background') {
+        if (type == "player" || type == "obstacle" || type == "background") {
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-            if (type == 'background') {
+            if (type == "background") {
                 ctx.drawImage(
                     this.image,
                     this.x + this.width,
@@ -109,6 +118,7 @@ function component(width, height, color, x, y, type, bufferNum) {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
+
     };
     // -------------------------------------------------------------------------Posicion del personaje
     this.newPos = function () {
@@ -117,7 +127,7 @@ function component(width, height, color, x, y, type, bufferNum) {
         //--------------------------------------------------Modificación doficultad juego con el avance
         if (puntos >= 400) {
             velocidad = 6;
-            dist = 1.6;
+            dificultad = 0.1;
         }
         if (puntos >= 800) {
             velocidad = 4.5;
@@ -132,7 +142,7 @@ function component(width, height, color, x, y, type, bufferNum) {
             dist = 1.9;
         }
         //------------------------------------------------------------------------------
-        if (this.type == 'player') {
+        if (this.type == "player") {
             if (this.x > 590) {
                 this.x = 590;
             }
@@ -146,7 +156,7 @@ function component(width, height, color, x, y, type, bufferNum) {
                 this.y = 0;
             }
         }
-        if (this.type == 'background') {
+        if (this.type == "background") {
             if (this.x == -this.width) {
                 this.x = 0;
             }
@@ -201,7 +211,7 @@ function updateGameArea() {
                 // `./images/juego2/obstacle3.png`,
                 x,
                 y,
-                'obstacle'
+                "obstacle"
             )
         );
     }
@@ -221,29 +231,29 @@ function updateGameArea() {
     personaje.speedX = 0;
     personaje.speedY = 0;
     if (
-        (gameArea.keys && gameArea.keys['ArrowLeft']) ||
-        (gameArea.keys && gameArea.keys['a'])
+        (gameArea.keys && gameArea.keys["ArrowLeft"]) ||
+        (gameArea.keys && gameArea.keys["a"])
     ) {
-        personaje.speedX = -dist;
+        personaje.speedX = (-dist - dificultad);
     }
 
     if (
-        (gameArea.keys && gameArea.keys['ArrowRight']) ||
-        (gameArea.keys && gameArea.keys['d'])
+        (gameArea.keys && gameArea.keys["ArrowRight"]) ||
+        (gameArea.keys && gameArea.keys["d"])
     ) {
-        personaje.speedX = dist;
+        personaje.speedX = (dist + dificultad);
     }
     if (
-        (gameArea.keys && gameArea.keys['ArrowUp']) ||
-        (gameArea.keys && gameArea.keys['w'])
+        (gameArea.keys && gameArea.keys["ArrowUp"]) ||
+        (gameArea.keys && gameArea.keys["w"])
     ) {
-        personaje.speedY = -dist;
+        personaje.speedY = (-dist - dificultad);
     }
     if (
-        (gameArea.keys && gameArea.keys['ArrowDown']) ||
-        (gameArea.keys && gameArea.keys['s'])
+        (gameArea.keys && gameArea.keys["ArrowDown"]) ||
+        (gameArea.keys && gameArea.keys["s"])
     ) {
-        personaje.speedY = dist;
+        personaje.speedY = (dist + dificultad);
     }
 
     personaje.newPos();
@@ -252,7 +262,7 @@ function updateGameArea() {
     //----------------------------------------------------------------------Generación buffers
     if (!bufferCond) {
         bufferCond = 1;
-        console.log('netra en condicion');
+        console.log("netra en condicion");
         setTimeout(bufferCreation, 5000);
     } else {
         bufferCreation();
@@ -271,7 +281,7 @@ function updateGameArea() {
                     `./images/juego2/buffer${bufferN}.svg`,
                     x,
                     y,
-                    'obstacle',
+                    "obstacle",
                     bufferN
                 )
             );
@@ -286,33 +296,57 @@ function updateGameArea() {
     for (i = 0; i < buffers.length; i += 1) {
         switch (buffers[i].bufferNum) {
             case 0:
-                if (personaje.collision(buffers[i])) {
+                if (personaje.collision(buffers[i])) { //buff velocidad
                     if (dist == 1.5) {
-                        speedModifier(1.5);
+                        speedModifier(2.5);
+                        console.log("speedmodifier " + dist);
                     }
                     break;
                 }
-            case 1:
-                if (personaje.collision(buffers[i])) {
-                    if (dist == 1.5) {
-                        speedModifier(1.5);
+                case 1:
+                    if (personaje.collision(buffers[i])) { //buff puntuación
+                        if (score == 1) {
+                            scoreModifier(4);
+                            console.log("scoremodifier " + score);
+                        }
+                        break;
                     }
-                    break;
-                }
-            case 2:
-                if (personaje.collision(buffers[i])) {
-                    if (dist == 1.5) {
-                        speedModifier(1.5);
-                    }
-                    break;
-                }
+                    case 2:
+                        if (personaje.collision(buffers[i])) { //buff tamaño
+                            if (sizeChange == 1) {
+                                sizeModifier(1 / 1.5);
+                            }
+                            break;
+                        }
+                        case 3:
+                            if (personaje.collision(buffers[i])) { //debuff velocidad
+                                if (dist == 1.5) {
+                                    speedModifier(1 / 2.5);
+                                }
+                                break;
+                            }
+                            case 4:
+                                if (personaje.collision(buffers[i])) { //debuff puntuación
+                                    if (score == 1) {
+                                        scoreModifier(0.5);
+                                    }
+                                    break;
+                                }
+                                case 5:
+                                    if (personaje.collision(buffers[i])) { //debuff tamaño
+                                        if (sizeChange == 1) {
+                                            sizeModifier(1.5);
+                                        }
+                                        break;
+                                    }
         }
     }
+    //console.log("velocidad "+dist+"; multiplicador "+score+"; tamaño"+sizeChange);
 }
 
 //---------------------------------------------------------------Cronómetro puntuación
 function iniciarcronometro() {
-    hora = setInterval(cronometro, 50 * score);
+    hora = setInterval(cronometro, (200 * score));
 }
 
 function pararcronometro() {
@@ -323,24 +357,24 @@ function pararcronometro() {
 function resetcronometro() {
     clearInterval(hora);
     puntos = 0;
-    document.querySelector('#cronoText').innerHTML = 'Puntos: ';
-    document.querySelector('#puntos').innerHTML = puntos;
+    document.querySelector("#cronoText").innerHTML = "Puntos: ";
+    document.querySelector("#puntos").innerHTML = puntos;
 }
 
 function cronometro() {
     puntos++;
-    document.querySelector('#puntos').innerHTML = puntos;
+    document.querySelector("#puntos").innerHTML = puntos;
 }
 
 //--------------------------------------------------------------------Leaderboard
 function checkleaderboard() {
     let checking = 0;
     if (window.localStorage.length) {
-        leaderplayer = JSON.parse(localStorage.getItem('jugadores'));
-        leaderscore = JSON.parse(localStorage.getItem('puntuaciones'));
+        leaderplayer = JSON.parse(localStorage.getItem("jugadores"));
+        leaderscore = JSON.parse(localStorage.getItem("puntuaciones"));
     } else {
-        leaderscore = ['', '', '', '', ''];
-        leaderplayer = ['', '', '', '', ''];
+        leaderscore = ["", "", "", "", ""];
+        leaderplayer = ["", "", "", "", ""];
     }
 
     for (let i = 0; i < 5; i++) {
@@ -355,44 +389,44 @@ function checkleaderboard() {
     }
 }
 
-let leaderboard = document.querySelector('#leaderboard');
+let leaderboard = document.querySelector("#leaderboard");
 
 function nameWinner() {
-    document.querySelector('#botoninicio').style.display = 'none';
-    let winnerInput = document.createElement('INPUT');
-    winnerInput.setAttribute('type', 'text');
-    winnerInput.setAttribute('id', 'winnerInput');
-    let winnerSubmit = document.createElement('INPUT');
-    winnerSubmit.setAttribute('type', 'submit');
-    winnerSubmit.setAttribute('id', 'winnerSubmit');
-    winnerSubmit.setAttribute('value', 'Incluye tu nombre');
+    document.querySelector("#botoninicio").style.display = "none";
+    let winnerInput = document.createElement("INPUT");
+    winnerInput.setAttribute("type", "text");
+    winnerInput.setAttribute("id", "winnerInput");
+    let winnerSubmit = document.createElement("INPUT");
+    winnerSubmit.setAttribute("type", "submit");
+    winnerSubmit.setAttribute("id", "winnerSubmit");
+    winnerSubmit.setAttribute("value", "Incluye tu nombre");
     leaderboard.appendChild(winnerInput);
     leaderboard.appendChild(winnerSubmit);
-    winnerSubmit.addEventListener('click', updatearleaderboard);
+    winnerSubmit.addEventListener("click", updatearleaderboard);
 }
 
 function displayLeaderboard() {
-    document.querySelector('#botoninicio').style.display = 'block';
+    document.querySelector("#botoninicio").style.display = "block";
 
-    let winnerInput = document.querySelector('#winnerInput');
-    let winnerSubmit = document.querySelector('#winnerSubmit');
+    let winnerInput = document.querySelector("#winnerInput");
+    let winnerSubmit = document.querySelector("#winnerSubmit");
     if (winnerInput && winnerSubmit) {
         leaderboard.removeChild(winnerInput);
         leaderboard.removeChild(winnerSubmit);
     }
 
-    let leadList = document.createElement('ol');
-    leadList.setAttribute('id', 'leadList');
+    let leadList = document.createElement("ol");
+    leadList.setAttribute("id", "leadList");
     for (let i = 0; i < 5; i++) {
-        if (leaderscore[i] != '') {
-            let leadPlayer = document.createElement('li');
-            let rowName = document.createElement('span');
+        if (leaderscore[i] != "") {
+            let leadPlayer = document.createElement("li");
+            let rowName = document.createElement("span");
             let spanName = document.createTextNode(leaderplayer[i]);
-            rowName.setAttribute('id', 'rowName');
+            rowName.setAttribute("id", "rowName");
             rowName.appendChild(spanName);
-            let rowScore = document.createElement('span');
+            let rowScore = document.createElement("span");
             let spanScore = document.createTextNode(leaderscore[i]);
-            rowScore.setAttribute('id', 'rowScore');
+            rowScore.setAttribute("id", "rowScore");
             rowScore.appendChild(spanScore);
             leadPlayer.appendChild(rowName);
             leadPlayer.appendChild(rowScore);
@@ -403,7 +437,7 @@ function displayLeaderboard() {
 }
 
 function updatearleaderboard() {
-    let playername = document.querySelector('#winnerInput').value;
+    let playername = document.querySelector("#winnerInput").value;
     let newlead = 0;
     let holderplayerin;
     let holderplayerout;
@@ -425,15 +459,15 @@ function updatearleaderboard() {
             leaderscore[i] = holderscoreout;
         }
     }
-    localStorage.setItem('puntuaciones', JSON.stringify(leaderscore));
-    localStorage.setItem('jugadores', JSON.stringify(leaderplayer));
+    localStorage.setItem("puntuaciones", JSON.stringify(leaderscore));
+    localStorage.setItem("jugadores", JSON.stringify(leaderplayer));
     showleaderboard();
     displayLeaderboard();
 }
 
 function showleaderboard() {
-    leaderplayer = JSON.parse(localStorage.getItem('jugadores'));
-    leaderscore = JSON.parse(localStorage.getItem('puntuaciones'));
+    leaderplayer = JSON.parse(localStorage.getItem("jugadores"));
+    leaderscore = JSON.parse(localStorage.getItem("puntuaciones"));
 }
 
 //-------------------------------------------------------Funciones powerups
@@ -442,8 +476,9 @@ function speedModifier(speedModifier) {
     //si es buffer le pasamos un valor >1 y si es debuffer <1
     registrodist = dist;
     dist = dist + speedModifier;
-    setTimeout(resetSpeed, 2000, registrodist);
+    setTimeout(resetSpeed, 4000, registrodist);
 }
+
 function resetSpeed(distoriginal) {
     dist = distoriginal;
 }
@@ -452,10 +487,30 @@ function resetSpeed(distoriginal) {
 function scoreModifier(scoreModifier) {
     //si es buffer le pasamos un valor >1 y si es debuffer <1
     score = score / scoreModifier;
-    setTimeout(resetScore, 2000);
-}
-function resetScore() {
-    score = 1;
+    clearInterval(hora);
+    iniciarcronometro();
+    setTimeout(resetScore, 4000);
 }
 
-document.querySelector('#botoninicio').addEventListener('click', startGame);
+function resetScore() {
+    score = 1;
+    clearInterval(hora);
+    iniciarcronometro();
+}
+
+//Buffer y debuffer tamaño
+function sizeModifier(sizeModifier) {
+    //si es buffer le pasamos un valor <1 y si es debuffer >1
+    sizeChange = sizeChange * sizeModifier;
+    personaje.width = personaje.width * sizeChange;
+    personaje.height = personaje.height * sizeChange;
+    setTimeout(resetSize, 4000);
+}
+
+function resetSize() {
+    sizeChange = 1;
+    personaje.width = 50;
+    personaje.height = 50;
+}
+
+document.querySelector("#botoninicio").addEventListener("click", startGame);
